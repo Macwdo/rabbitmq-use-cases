819778973e6aff3e20d505d7e755c156e89f9fe3
Macwdo <danilo.macedofernandes@hotmail.com>
2023-09-15 08:52:19 -0300

    :sparkles: Competing consumers implementation using C#

---
+++ C#/CompetingConsumers/Consumer/ConsumerRaw.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null     
+);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);;
+
+var consumer = new EventingBasicConsumer(channel);
+
+var random = new Random();
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    var timeToProcess = random.Next(1, 3);
+        
+    Console.WriteLine($"Received: {message}");
+    Task.Delay(TimeSpan.FromSeconds(timeToProcess)).Wait();
+    Console.WriteLine($"Finished: {timeToProcess}");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(
+    queue: "letterbox",
+    autoAck: false,
+    consumer: consumer
+);
+
+Console.ReadKey();
+
---
+++ C#/CompetingConsumers/Producer/ProducerRaw.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null     
+);
+
+int messageID = 0;
+var random =  new Random();
+
+
+while (true){
+
+    var publishingTime = random.Next(1, 3);
+
+    var message = $"Sending message: MessageID[{messageID}]";
+    var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+    channel.BasicPublish("", "letterbox", null, encodedMessage);
+    Console.WriteLine($"Send: {message}");
+    
+    Task.Delay(TimeSpan.FromSeconds(publishingTime)).Wait();
+
+    messageID++;
+}
+
