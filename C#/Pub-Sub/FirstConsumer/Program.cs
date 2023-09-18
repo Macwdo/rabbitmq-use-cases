e4dd0c22d2a70253fb3f14a9f58d623d9afc993c
Macwdo <Macwdo>
2023-09-18 08:15:53 -0300

    :sparkles: Pub/sub pattern implementation using c#

---
+++ C#/Pub-Sub/FirstConsumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+
+var factory = new ConnectionFactory{ HostName = "localhost"};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "pub-sub", ExchangeType.Fanout);
+
+var queue = channel.QueueDeclare(queue: "", exclusive: true);
+var queueName = queue.QueueName;
+
+channel.QueueBind(queue: queueName, exchange: "pub-sub", routingKey: "");
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) => {
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"First Consumer - Received: {message}");
+};
+
+channel.BasicConsume(queue: queueName, autoAck: true, consumer);
+Console.ReadKey();
---
+++ C#/Pub-Sub/Producer/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory{ HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.ExchangeDeclare(exchange: "pub-sub", type: ExchangeType.Fanout);
+
+var message = "Hello i want broadcast this message!";
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish(exchange: "pub-sub", routingKey: "", basicProperties: null, body: encodedMessage);
+
+Console.WriteLine($"Published Message '{message}'");
